2b398cdf7ccdb010eda38285990f0991bc077242
ashcrypto03 <143936365+ashcrypto03@users.noreply.github.com>
2025-10-08 17:25:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Floating widget toggle
+const chatButton = document.getElementById("chatButton");
+const chatWidget = document.getElementById("chatWidget");
+const closeChat = document.getElementById("closeChat");
+
+chatButton.addEventListener("click", () => {
+  chatWidget.classList.toggle("active");
+});
+
+closeChat.addEventListener("click", () => {
+  chatWidget.classList.remove("active");
+});
+
 const input = document.getElementById("chatInput");
 const sendBtn = document.getElementById("sendBtn");
 const chatBody = document.getElementById("chatBody");
